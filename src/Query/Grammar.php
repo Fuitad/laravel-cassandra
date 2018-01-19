152a48cd4be922a89b64e8f9be3e1ddc72bc89d6
Roman Hrab <lroman242@gmail.com>
2018-01-19 19:30:21 +0200

    simple cql grammar

--- src/Query/Grammar.php
+++ src/Query/Grammar.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Query\Grammars\Grammar as BaseGrammar;
 
 class Grammar extends BaseGrammar
 {
+    /**
+     * The components that make up a select clause.
+     *
+     * @var array
+     */
+    protected $selectComponents = [
+        'aggregate',
+        'columns',
+        'from',
+        'wheres',
+        'orders',
+        'limit',
+        'allowFiltering'
+    ];
 
+    public function compileAllowFiltering(Builder $query, $bool)
+    {
+        return (bool) $bool ? 'ALLOW FILTERING' : '';
+    }
 }
